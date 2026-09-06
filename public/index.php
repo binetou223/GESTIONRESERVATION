367b2a1e4d5336ddc579158c0f1d76c5d8948e07
Binetou <binetoudiasn@gmail.com>
2026-09-06 21:12:35 +0000

    chore:ajout des fichier env et configuration de Capsule/Manager et demarage de Eloquent

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$capsule = require_once dirname(__DIR__) . '/config/database.php';
+
+try {
+    $capsule->getConnection()->getPdo();
+
+    echo "Connexion à la base de données réussie !";
+} catch (\Throwable $e) {
+    echo "Erreur de connexion à la base de données.";
+}
