1988a53c8a536dec87300dd20624c0f21e8fcf6e
Binetou <binetoudiasn@gmail.com>
2026-09-06 21:47:06 +0000

    chore:creation des tables

---
+++ database/migration/002_creation_reservation.php
@@ -0,0 +1,21 @@
+<?php
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
+require_once __DIR__ . '/../../vendor/autoload.php';
+$capsule = require_once dirname(__DIR__) . '/../config/database.php';
+if (!$capsule->schema()->hasTable('reservation')) {
+    $capsule->schema()->create('reservation', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('nom_utilisateur');
+        $table->string('email_utilisateur');
+        $table->string('motif_reservation');
+        $table->dateTime('date_debut');
+        $table->dateTime('date_fin');
+        $table->timestamps();
+        $table->string('status')->default('confirme');
+        $table->foreignId('salle_id')->constrained('salle');
+    });
+    echo "Table 'reservation' créée avec succès.";
+} else {
+    echo "La table 'reservation' existe déjà.";
+}
